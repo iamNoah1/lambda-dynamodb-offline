af520a4182ee07004e2b168585cf5d33098af8ee
mebnoah <noahispas@gmail.com>
2017-10-25 17:51:19 +0200

    fixed linting

--- handler.js
+++ handler.js
@@ -1,7 +1,7 @@
-const AWS = require('aws-sdk');
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const AWS = require('aws-sdk')
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
 
-const uuid = require('uuid');
+const uuid = require('uuid')
 
 module.exports.write = (event, context, callback) => {
   const data = JSON.parse(event.body)
@@ -11,48 +11,47 @@ module.exports.write = (event, context, callback) => {
       id: uuid.v1(),
       text: data.text
     }
-  };
+  }
 
   dynamoDb.put(params, (error) => {
     if (error) {
-      console.error(error);
+      console.error(error)
       callback(null, {
         statusCode: error.statusCode || 501,
         headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t create the todo item.',
-      });
-      return;
+        body: 'Couldn\'t create the todo item.'
+      })
+      return
     }
 
     const response = {
       statusCode: 200,
-      body: JSON.stringify(params.Item),
-    };
-    callback(null, response);
-  });
-};
+      body: JSON.stringify(params.Item)
+    }
+    callback(null, response)
+  })
+}
 
 module.exports.read = (event, context, callback) => {
   const params = {
-    TableName: process.env.DYNAMODB_TABLE,
-  };
+    TableName: process.env.DYNAMODB_TABLE
+  }
 
   dynamoDb.scan(params, (error, result) => {
     if (error) {
-      console.error(error);
+      console.error(error)
       callback(null, {
         statusCode: error.statusCode || 501,
         headers: { 'Content-Type': 'text/plain' },
-        body: 'Couldn\'t fetch the todos.',
-      });
-      return;
+        body: 'Couldn\'t fetch the todos.'
+      })
+      return
     }
 
     const response = {
       statusCode: 200,
-      body: JSON.stringify(result.Items),
-    };
-    callback(null, response);
-  });
-
-};
+      body: JSON.stringify(result.Items)
+    }
+    callback(null, response)
+  })
+}
